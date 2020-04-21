25a77ebae529da0de2da4f63727d60276d41c55e
Shubhamsg7 <59820080+Shubhamsg7@users.noreply.github.com>
2020-04-21 19:41:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
+
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
@@ -17,3 +18,31 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+
+
+function generate() {
+
+	var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+	
+	function populate(a) {
+	  for ( var i = 0; i < 6; i++ ) {
+		var x = Math.round( Math.random() * 14 );
+		var y = hexValues[x];
+		a += y;
+	  }
+	  return a;
+	}
+	
+	var newColor1 = populate('#');
+	var newColor2 = populate('#');
+	var angle = Math.round( Math.random() * 360 );
+	
+	var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+	
+	document.getElementById("gradient").style.background = gradient;
+	document.getElementById("code").innerHTML = gradient;
+	
+	}
+	
+	document.onload = generate();
